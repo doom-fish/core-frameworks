d97fe0cb065823af701d7e7b3a688d71ae055d2e
Per Johansson <mail@perjohansson.net>
2024-12-13 14:45:02 +0100

    fix(CMVideoCodecType): enable CMVideoCodecType again

--- crates/core-media-rs/src/lib.rs
+++ crates/core-media-rs/src/lib.rs
@@ -4,5 +4,5 @@ pub mod cm_format_description;
 pub mod cm_sample_buffer;
 pub mod cm_sample_timing_info;
 pub mod cm_time;
-// pub mod cm_video_codec_type;
+pub mod cm_video_codec_type;
 pub mod types;
